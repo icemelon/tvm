2647f95a96af6abfda5c56d3cc12a1427b8f5721
Haichen Shen <shenhaichen@gmail.com>
2019-04-26 11:48:12 -0700

    Move mxnet control test out of test_vm

---
+++ tests/python/frontend/mxnet/test_control.py
@@ -0,0 +1,132 @@
+import os
+import mxnet as mx
+from mxnet import gluon
+import tvm
+from tvm import relay
+from tvm.relay.vm import _eval_vm, eta_expand
+
+def import_mxnet_model(fname, num_states):
+    ctx = mx.context.cpu()
+    data_names = ['data0']
+    for i in range(num_states):
+        data_names.append('data%s' % (i+1))
+
+    model_data_dir = os.path.dirname(os.path.realpath(__file__))
+    net = gluon.nn.SymbolBlock.imports("%s/model_zoo_data/%s-symbol.json.data" % (model_data_dir, fname), data_names,
+                                       "%s/model_zoo_data/%s-0001.params.data" % (model_data_dir, fname), ctx=ctx)
+    net.hybridize()
+    return net
+
+def test_rnn(cell_type):
+    input_size = 128
+    hidden_size = 128
+    batch, seq_len = 1, tvm.var('seq_len')
+    data_shape= (seq_len, batch, input_size)
+    state_shape = (batch, hidden_size)
+    num_states = 2 if cell_type == 'lstm' else 1
+    mx_net = import_mxnet_model("%s_i128_h128" % cell_type, num_states)
+
+    shapes = {'data': data_shape}
+    mx_input_syms = []
+    mx_input_syms.append(mx.sym.Variable("data"))
+    for i in range(num_states):
+        shapes['state%s' % i] = state_shape
+        mx_input_syms.append(mx.sym.Variable("state%s" % i))
+
+    mod = relay.module.Module()
+    relay_net, params = relay.frontend.from_mxnet(mx_net, shape=shapes, input_symbols=mx_input_syms, module=mod)
+    params = params.items()
+
+    loop = None
+    for v, func in mod.functions.items():
+        if v.name_hint == 'foreach':
+            loop = v
+            print(relay.ir_pass.infer_type(func, mod=mod))
+            # print("func params: {}".format(func.params))
+
+    inputs = [relay.var('data')]
+    for i in range(num_states):
+        inputs.append(relay.var('state%s' % i))
+    for name, _ in params:
+        inputs.append(relay.var(name))
+    mod[mod.entry_func] = relay.Function(inputs, relay.Call(relay_net, inputs))
+    print(relay.ir_pass.infer_type(mod[mod.entry_func], mod=mod))
+
+    l = 5
+    data_v = np.random.rand(l, batch, 128).astype('float32')
+    states_v = [np.random.rand(*state_shape).astype('float32') for _ in range(num_states)]
+    params_v = [e[1].asnumpy() for e in params]
+    print('eval vm')
+    result = _eval_vm(mod, tvm.cpu(), data_v, *states_v, *params_v)
+    print("Relay result is {}".format(result))
+
+    mx_inputs = [mx.nd.array(x) for x in [data_v, *states_v]]
+    mx_outputs = mx_net(*mx_inputs)
+    print("======== MXNet result ==========")
+    for o in mx_outputs:
+        print("MXNet output : {}".format(o.asnumpy()))
+
+def test_while():
+    n = 5
+    class Scan(gluon.HybridBlock):
+        def hybrid_forward(self, F, data):
+            def sum(state, i):
+                s = state + F.take(data, i)
+                return [s], [s, i + 1]
+            def sum_cond(state, i):
+                return i < n
+            out, state = F.contrib.while_loop(sum_cond, sum,
+                                              [F.zeros((1)), F.zeros((1))], max_iterations=5)
+            return out, state
+
+    data = mx.nd.arange(n)
+    scan_layer = Scan()
+    scan_layer.hybridize()
+    scan_layer(data)
+    mx_sym = scan_layer._cached_graph[1]
+    mod = relay.module.Module()
+    sym, _ = relay.frontend.from_mxnet(mx_sym, shape={'data': (5,)}, module=mod)
+    print(sym)
+
+    for v, func in mod.functions.items():
+        if v.name_hint == 'while_loop':
+            print(relay.ir_pass.infer_type(func, mod=mod))
+
+    inputs = [relay.var('data')]
+    mod[mod.entry_func] = relay.Function(inputs, relay.Call(sym, inputs))
+    print(relay.ir_pass.infer_type(mod[mod.entry_func], mod=mod))
+
+    data_np = np.arange(n).astype('float32')
+
+    print('eval interpreter')
+    intrp = relay.create_executor("debug", mod=mod, ctx=tvm.cpu(), target="llvm")
+    op_res = intrp.evaluate(mod.entry_func)(data_np)
+    print("Interpreter result is {}".format(op_res))
+
+    print('eval vm')
+    result = _eval_vm(mod, tvm.cpu(), data_np)
+    print("Relay result is {}".format(result))
+
+    mx_inputs = [mx.nd.array(data_np)]
+    mx_outputs = scan_layer(*mx_inputs)
+    print("======== MXNet result ==========")
+    for o in mx_outputs:
+        print("MXNet output : {}".format(o))
+
+
+def test_cond():
+    x = mx.sym.var('x')
+    y = mx.sym.var('y')
+    z = mx.sym.var('z')
+    def then_func():
+        return x + z
+    def else_func():
+        return mx.sym.square(y)
+    out = mx.sym.contrib.cond(mx.sym.min(x) < mx.sym.min(y), then_func, else_func)
+
+    relay.frontend.from_mxnet(out)
+
+if __name__ == "__main__":
+    # test_rnn('lstm')
+    # test_while()
+    test_cond()
